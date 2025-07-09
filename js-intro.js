a2a2bf1bbb75fab55d30ed038b780b1f6301574d
sadekul-ai <sadekul.psl@gmail.com>
2025-07-09 08:43:08 +0600

    Add files via upload

---
+++ js-intro.js
@@ -0,0 +1,148 @@
+
+var a = 20;
+//console.log(a);
+
+var name = "Sadekul";
+//console.log(name);
+
+var a = 20;
+var b =30;
+//console.log(a + b);
+
+var a = 20;
+var b =30;
+//console.log(b-a);
+
+var isSunny = false;
+//console.log(isSunny);
+
+var productPrice = 30;
+//console.log(productPrice);
+
+var batajuta = 99.99;
+var convertPrice = parseInt(batajuta);
+//console.log(convertPrice);
+
+var batajuta = "99.99";
+var convertPrice = parseInt(batajuta);
+//console.log(convertPrice);
+
+var batajuta = "99.99";
+var number = parseFloat(batajuta);
+//console.log(number);
+
+var egg = 20;
+var apple = 50;
+var sumOfEggAndApple = egg + apple;
+//console.log(sumOfEggAndApple);
+
+var egg = 30;
+var apple = 50;
+var total = egg + apple;
+//console.log(total);
+
+var egg = 30;
+var apple = 40;
+var applePrice = parseFloat(apple);
+var total = egg + applePrice;
+//console.log(total);
+
+var egg = 30;
+var apple = 40.50;
+var applePrice = parseFloat(apple);
+var total = egg + applePrice;
+//console.log(total);
+
+var egg = 30;
+var apple = 40.50;
+var applePrice = parseInt(apple);
+var total = egg + applePrice;
+//console.log(total);
+
+var onion = 150;
+//console.log(onion);
+
+var onion = 150;
+onion = 80;
+//console.log(onion);
+
+var school = "Shimultoli Primary School";
+//console.log(school);
+
+var school = "Shimultoli Primary School";
+school = "South Polashbari High School";
+//console.log(school);
+
+var sugar = 50;
+var spoon = 30;
+var substractSugarFromSpoon = sugar-spoon;
+//console.log(substractSugarFromSpoon);
+
+var sugar = 20;
+var spoon = 3;
+var multiplyBetweenSugarAndSpoon = sugar*spoon;
+//console.log(multiplyBetweenSugarAndSpoon);
+
+var sugar = 30;
+var spoon = 3;
+var dividedToSugarBySpoon = sugar/spoon;
+//console.log(dividedToSugarBySpoon);
+
+var age = 50;
+//console.log(age);
+
+var age = 50;
+age = age + 1;
+//console.log(age);
+
+var age = 40;
+age += 1;
+//console.log(age);
+
+var age = 40;
+age ++;
+//console.log(age);
+
+var age = 40;
+age ++;
+age ++;
+//console.log(age);
+
+var age = 40;
+age ++
+age ++
+age ++
+//console.log(age);
+
+var seconds = 60;
+//console.log(seconds);
+
+var seconds = 60;
+seconds = seconds-1;
+//console.log(seconds);
+
+var seconds = 60;
+seconds -= 1;
+//console.log(seconds);
+
+var seconds = 60;
+seconds = seconds-10;
+//console.log(seconds);
+
+var seconds = 60;
+seconds--
+//console.log(seconds);
+
+var seconds = 60;
+seconds--
+seconds--
+seconds--
+//console.log(seconds);
+
+
+
+
+
+
+
+
